3e5bcd1a6d3b6c24e95d5045b7ddd577a9155590
Blackfox-Thane <107892323+Blackfox-Thane@users.noreply.github.com>
2026-08-13 21:51:08 -0400

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,48 +7,47 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 sudo pacman -Syu --noconfirm \
-cmake \
-ninja \
-clang \
-python \
-sdl2 \
-glew \
-zlib \
-bzip2 \
-libzip \
-nlohmann-json \
-tinyxml2 \
-spdlog
+  cmake \
+  ninja \
+  clang \
+  python \
+  sdl2 \
+  glew \
+  zlib \
+  bzip2 \
+  libzip \
+  nlohmann-json \
+  tinyxml2 \
+  python-pyyaml \
+  python-pillow \
+  spdlog
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+./get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package zenity-rs-bin
+# sudo ./make-aur-package zenity-rs-bin
 
 # If the application needs to be manually built that has to be done down here
 
 echo "Getting app for G-Diffuser..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/Zorkats/G-Diffuser"
-
 VERSION="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//')"
 git clone --recursive "$REPO" ./G-Diffuser
 echo "$VERSION" > ~/version
 
-mkdir -p ./AppDir/bin
+mkdir -p AppDir/bin
 cd G-Diffuser
 mkdir -p build/x64-linux
 
-cmake -S . -Bbuild/x64-linux  \
--GNinja \
--DCMAKE_BUILD_TYPE=Release \
--DASSET_REVISION=rev0 \
--DASSET_VERSION=us \
--DEXPANSION_KIT=1
+cmake -S . \
+  -Bbuild/x64-linux \
+  -GNinja \
+  -DCMAKE_BUILD_TYPE=Release
 
-# cmake --build build/x64-linux --target assets
+# cmake --build build/x64-linux --target include/assets
 
 cmake --build build/x64-linux --target G-Diffuser
 
